eee90def99a640e30fd30f5d26f00857185d87a9
Faith Mukumbiri <qfaffy@gmail.com>
2023-05-19 13:34:30 -0700

    Path file

---
+++ exec.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+
+/**
+ * find_executable_path - Find the path to an executable file
+ * @command: The command to find
+ * @env_vars: The environment variables
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int find_executable_path(char **command, char **env_vars)
+{
+	char *token = NULL, *path_str = NULL, *abs_path = NULL;
+	size_t path_len, arg_len;
+	struct stat file_stat;
+
+	if (stat(*command, &file_stat) == 0)
+		return (-1);
+	path_str = get_path_string(env_vars); /*gets the content of "PATH="*/
+	if (!path_str)
+		return (-1);
+	token = strtok(path_str, ":"); /** parses the content of "PATH="*/
+	arg_len = string_length(command); /*gets length of arg*/
+	while (token)
+	{
+		path_len = string_length(token);
+		abs_path = malloc(sizeof(char) * (path_len + arg_len + 2));
+		if (!abs_path)
+		{
+			free(path_str);
+			return (-1);
+		}
+
+		abs_path = strcpy(abs_path, token);
+		string_concat(abs_path, "/");
+		string_concat(abs_path, *command);
+
+		if (stat(abs_path, &file_stat) == 0)
+		{
+			*commanc = abs_path;
+			free(path_str);
+			return (0);
+		}
+
+		free(abs_path);
+		token = strtok(NULL, ":");
+	}
+
+	token = '\0';
+	free(path_str);
+	return (-1);
+}
+
